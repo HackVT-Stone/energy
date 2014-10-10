5382e4d29b95af8e5b9669032caf7ddd8e6e4a9a
Nick Floersch <tech@stone-env.com>
2014-10-10 12:25:08 -0400

    Starter Page for JS N00bs
    Because we have three team members that know little about JS… this page
    and associated JS include file will help them get started.

---
+++ sample_code/myInclude.js
@@ -0,0 +1,6 @@
+// This file is included in the starter page and is the best place to start
+// creating your own functions.
+
+function writeToPage(param) {
+	document.getElementById("outputDiv").innerHTML = param;	
+	}
