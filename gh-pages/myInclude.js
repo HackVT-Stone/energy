28ea5b03d0fa079433ce4cb4f01c8bac118c76db
Nick Floersch <tech@stone-env.com>
2014-10-10 12:32:07 -0400

    Did GH Pages wrong... fixing

--- gh-pages/myInclude.js
+++
@@ -1,6 +0,0 @@
-// This file is included in the starter page and is the best place to start
-// creating your own functions.
-
-function writeToPage(param) {
-	document.getElementById("outputDiv").innerHTML = param;	
-	}
